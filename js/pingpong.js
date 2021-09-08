a8039c22bf94650f6b129eed83a2d035aa972303
chantifa <chantifa@windowslive.com>
2021-09-08 22:15:55 +0200

    Initial commit

---
+++ js/pingpong.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+// alert a message
+        alert("Welcome to the Ping Pong battle.");
+    });
+})(jQuery);
+
+/*
+jQuery(document).ready(function(){
+// code here.
+});
+ */
